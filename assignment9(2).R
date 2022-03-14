5e5ce9d90307c098ad25273fc8bca619bb4c5ded
sudeepthi5 <97864497+sudeepthi5@users.noreply.github.com>
2022-03-14 00:13:31 -0400

    Update assignment9(2).R

--- assignment9(2).R
+++ assignment9(2).R
@@ -1,17 +1,27 @@
 plot(Affairs)
+##the choosen dataset affairs is plotted.
 
 df= data.frame(Affairs)
+##dataset Affairs is modified to dataframe
 
 boxplot(df$education~df$age)
+##basic visualization is done using boxplot command
 
 install.packages("lattice")
 library(lattice)
+##installed required lattice packages to create a lattice visualization plot
 
 tplot= xyplot(age~education, data = Affairs, pch ="+")
+## lattice plot is created using this command
+
 tplot
+##returns the plot created using lattice
 
 install.packages("ggplot2")
 library(ggplot2)
+##installed required ggplot2 packages
 
 f = ggplot(Affairs, aes(age, education)) + geom_point()
 f + geom_vline(xintercept = 4)
+
+## this command is used to create a ggplot of selected data.
